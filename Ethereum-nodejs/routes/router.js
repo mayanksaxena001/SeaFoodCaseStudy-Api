f786cf3741f349035f0bf25753a2af46c8a0f50b
mayanksaxena001 <mayanksaxena001@gmail.com>
2019-09-12 22:36:29 +0530

    routes added

--- Ethereum-nodejs/routes/router.js
+++ Ethereum-nodejs/routes/router.js
@@ -1,15 +1,13 @@
 var AssetContractApi = require('./assetContract.router');
 var AuthApi = require('./auth.router');
 // var Api = require('./contract.router');
-var SensorApi = require('./sensor.router');
-var WalletApi = require('./wallet.router');
-var TokenApi = require('./token.router');
-// var express = require('express');
-// const router = express.Router();
+var Sensor = require('./sensor.router');
+var Wallet = require('./wallet.router');
+var express = require('express');
+const router = express.Router();
 module.exports = (app) => {
-    app.use('/api/auth', AuthApi);
-    app.use('/contract', AssetContractApi);
-    app.use('/sensor', SensorApi);
-    app.use('/wallet', WalletApi);
-    app.use('/token', TokenApi);
+    app.use('/api/auth', Auth);
+    app.use('/api/contract', AssetContract);
+    app.use('/api/sensor', Sensor);
+    app.use('/api/wallet/', Wallet);
 }
